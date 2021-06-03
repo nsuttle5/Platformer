7e298af033c46e150a8e42cd95b6b2e75b1026e5
nsuttle5 <84676645+nsuttle5@users.noreply.github.com>
2021-06-03 18:11:53 -0400

    Add empty project

---
+++ 1stPlatformer - Copy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    private Rigidbody2D myRigidBody;
+
+    public float jumpSpeed;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded;
+
+    private Animator myAnim;
+
+    public Vector3 respawnPosition;
+
+    public LevelManager theLevelManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+
+        respawnPosition = transform.position;
+
+        theLevelManager = FindObjectOfType<LevelManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+
+        if(Input.GetAxisRaw ("Horizontal") > 0f)
+        {
+            myRigidBody.velocity = new Vector3(moveSpeed, myRigidBody.velocity.y, 0f);
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            myRigidBody.velocity = new Vector3(-moveSpeed, myRigidBody.velocity.y, 0f);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        else
+        {
+            myRigidBody.velocity = new Vector3(0f, myRigidBody.velocity.y, 0f);
+        }
+
+        if(Input.GetButtonDown ("Jump") && (isGrounded))
+        {
+            myRigidBody.velocity = new Vector3(myRigidBody.velocity.x, jumpSpeed, 0f);
+        }
+
+        myAnim.SetFloat("Speed", Mathf.Abs(myRigidBody.velocity.x));
+        myAnim.SetBool("Grounded", isGrounded);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if(other.tag == "KillPlane")
+        {
+           // gameObject.SetActive(false);
+
+            transform.position = respawnPosition;
+
+            theLevelManager.Respawn();
+        }
+
+        if(other.tag == "Checkpoint")
+        {
+            respawnPosition = other.transform.position;
+
+        }
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "MovingPlatform")
+        {
+            transform.parent = other.transform;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "MovingPlatform")
+        {
+            transform.parent = null;
+        }
+    }
+
+}
